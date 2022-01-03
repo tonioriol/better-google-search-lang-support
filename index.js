88dbdc74c5671b9362bdeb06310fe530a201a5cf
Toni Oriol <tonioriol@gmail.com>
2022-01-03 12:44:33 +0100

    fix: add regex, so it only shows in google search
    Proper desc describing current scope of functionality.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 // @description:en Add language filter in Google search for each language defined in langList var.
 // @version 3.0.0
 // @grant none
-// @include *.google.*
+// @include /^http(s)?:\/\/(www)?\.google\.\w*\/search.*$/
+// @namespace https://greasyfork.org/users/320969
+// @description It takes the languages you have configured and adds them as a separated filter at the top of the page.
 // ==/UserScript==
 (function () {
   const langList = ['ca', 'es', 'en', 'sv']
